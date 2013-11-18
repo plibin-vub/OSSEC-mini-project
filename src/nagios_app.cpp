385ca9e7a2b2d92e17b288ffd770fceee6987db1
Pieter Libin <pieter.libin@rega.kuleuven.be>
2013-11-18 16:19:46 +0100

    changes

--- src/nagios_app.cpp
+++ src/nagios_app.cpp
@@ -1,6 +1,67 @@
-#include <iostream>
-#include <ctime>
+//based on the daemon code found on:
+//http://stackoverflow.com/questions/17954432/creating-a-daemon-in-linux
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <syslog.h>
+#include <string>
+
+static void skeleton_daemon()
+{
+    pid_t pid;
+
+    /* Fork off the parent process */
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    /* On success: The child process becomes session leader */
+    if (setsid() < 0)
+        exit(EXIT_FAILURE);
+
+    /* Catch, ignore and handle signals */
+    //TODO: Implement a working signal handler */
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+
+    /* Fork off for the second time*/
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    /* Set new file permissions */
+    umask(0);
+
+    /* Change the working directory to the root directory */
+    /* or another appropriated directory */
+    chdir("/");
+
+    /* Close all open file descriptors */
+    int x;
+    for (x = sysconf(_SC_OPEN_MAX); x>0; x--)
+    {
+        close (x);
+    }
+
+    /* Open the log file */
+    openlog ("nagios_app", LOG_PID, LOG_DAEMON);
+}
 
 //from: http://stackoverflow.com/questions/997946/how-to-get-current-time-and-date-in-c
 const std::string currentDateTime() {
@@ -15,33 +76,29 @@ const std::string currentDateTime() {
     return buf;
 }
 
-int main () {
-  //Set our Logging Mask and open the Log
-  setlogmask(LOG_UPTO(LOG_NOTICE));
-  openlog("nagios_app", LOG_CONS | LOG_NDELAY | LOG_PERROR | LOG_PID, LOG_USER);
-  
-  int i=fork();
-  if (i<0) exit(1); /* fork error */
-  if (i>0) exit(0); /* parent exits */
-  /* child (daemon) continues */
+int main()
+{
+    skeleton_daemon();
 
-  clock_t begin = clock();
-  
-  while (true) {
-    double elapsed_secs = double(clock() - begin) / CLOCKS_PER_SEC;
-
-    //do some artificial transaction computations
-    for (unsigned i = 0; 
-	 i < 100000000;
-	 i++ ) {
-      double tmp = i * i;
+    clock_t begin = clock();
+    
+    while (true) {
+      double elapsed_secs = double(clock() - begin) / CLOCKS_PER_SEC;
+      
+      //do some artificial transaction computations
+      for (unsigned i = 0; 
+	   i < 100000000;
+	   i++ ) {
+	double tmp = i * i;
+      }
+      
+      if (elapsed_secs < 5 * 60 * 60) /*5 minutes*/ {
+	std::string log = std::string("Processed transaction ") + currentDateTime();
+	syslog(LOG_INFO, log.c_str());
+      }
     }
 
-    if (elapsed_secs < 5 * 60 * 60) //5 minutes
-      syslog(LOG_INFO, (std::string("Processed transaction ") + currentDateTime()).c_str());
-  }
+    closelog();
 
-  closelog();
-
-  exit(0);
+    return EXIT_SUCCESS;
 }
