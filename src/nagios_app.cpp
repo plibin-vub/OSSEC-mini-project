b122d927615967985517392a7c8b9b1f74bca007
Pieter Libin <pieter.libin@rega.kuleuven.be>
2013-11-18 16:32:43 +0100

    reverting

--- src/nagios_app.cpp
+++ src/nagios_app.cpp
@@ -1,67 +1,5 @@
-//based on the daemon code found on:
-//http://stackoverflow.com/questions/17954432/creating-a-daemon-in-linux
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <syslog.h>
-#include <string>
-
-static void skeleton_daemon()
-{
-    pid_t pid;
-
-    /* Fork off the parent process */
-    pid = fork();
-
-    /* An error occurred */
-    if (pid < 0)
-        exit(EXIT_FAILURE);
-
-    /* Success: Let the parent terminate */
-    if (pid > 0)
-        exit(EXIT_SUCCESS);
-
-    /* On success: The child process becomes session leader */
-    if (setsid() < 0)
-        exit(EXIT_FAILURE);
-
-    /* Catch, ignore and handle signals */
-    //TODO: Implement a working signal handler */
-    signal(SIGCHLD, SIG_IGN);
-    signal(SIGHUP, SIG_IGN);
-
-    /* Fork off for the second time*/
-    pid = fork();
-
-    /* An error occurred */
-    if (pid < 0)
-        exit(EXIT_FAILURE);
-
-    /* Success: Let the parent terminate */
-    if (pid > 0)
-        exit(EXIT_SUCCESS);
-
-    /* Set new file permissions */
-    umask(0);
-
-    /* Change the working directory to the root directory */
-    /* or another appropriated directory */
-    chdir("/");
-
-    /* Close all open file descriptors */
-    int x;
-    for (x = sysconf(_SC_OPEN_MAX); x>0; x--)
-    {
-        close (x);
-    }
-
-    /* Open the log file */
-    openlog ("nagios_app", LOG_PID, LOG_DAEMON);
-}
+#include <iostream>
+#include <ctime>
 
 //from: http://stackoverflow.com/questions/997946/how-to-get-current-time-and-date-in-c
 const std::string currentDateTime() {
@@ -76,29 +14,26 @@ const std::string currentDateTime() {
     return buf;
 }
 
-int main()
-{
-    skeleton_daemon();
+void log_transaction() {
+  std::cout << "Processed transaction " << currentDateTime() << std::endl; 
+}
 
-    clock_t begin = clock();
-    
-    while (true) {
-      double elapsed_secs = double(clock() - begin) / CLOCKS_PER_SEC;
-      
-      //do some artificial transaction computations
-      for (unsigned i = 0; 
-	   i < 100000000;
-	   i++ ) {
-	double tmp = i * i;
-      }
-      
-      if (elapsed_secs < 5 * 60 * 60) /*5 minutes*/ {
-	std::string log = std::string("Processed transaction ") + currentDateTime();
-	syslog(LOG_INFO, log.c_str());
-      }
+int main () {
+  clock_t begin = clock();
+  
+  while (true) {
+    double elapsed_secs = double(clock() - begin) / CLOCKS_PER_SEC;
+
+    //do some artificial transaction computations
+    for (unsigned i = 0; 
+	 i < 100000000;
+	 i++ ) {
+      double tmp = i * i;
     }
 
-    closelog();
+    if (elapsed_secs < 5 * 60 * 60) //5 minutes
+      log_transaction();
+  }
 
-    return EXIT_SUCCESS;
+  return 0;
 }
