fd3abd97eeb071690afb9078a13dbb50bb0d65dd
Pieter Libin <pieter.libin@rega.kuleuven.be>
2014-01-02 17:26:59 +0100

    add shard java project

---
+++ src/shard/src/ossec/shards/Actions.java
@@ -0,0 +1,117 @@
+package ossec.shards;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Actions {
+	static void addUser(String name) {
+		String targetShard = MapShards.whichShard(name);
+		
+		try {
+			String sql = "INSERT INTO user (name) VALUES (?)";
+			PreparedStatement s = createConnection(targetShard).prepareStatement(sql);
+			s.setString(0, name);
+			s.execute();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	static void post(String name, String message) {
+		String targetShard = MapShards.whichShard(name);
+		
+		try {
+			String sql = "INSERT INTO post (name, message) VALUES (?,?)";
+			PreparedStatement s = createConnection(targetShard).prepareStatement(sql);
+			s.setString(0, name);
+			s.setString(1, message);
+			s.execute();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	static List<String> findUsersThatPostedPattern(String pattern) {
+		List<String> users = new ArrayList<String>();
+		
+		for (String shard : MapShards.allShards()) {
+			users.addAll(findUsersThatPostedPattern(shard, pattern));
+		}
+		
+		return users;
+	}	
+	
+	private static List<String> findUsersThatPostedPattern(String shard, String pattern) {
+		List<String> users = new ArrayList<String>();
+		
+		try {
+			String sql = "SELECT * FROM post WHERE message LIKE ?";
+			PreparedStatement s = createConnection(shard).prepareStatement(sql);
+			s.setString(0, "%" + pattern + "%");
+			ResultSet rs = s.executeQuery();
+			while(rs.next()) {
+				String user = rs.getString("name");
+				users.add(user);
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return users;
+	}
+	
+	static List<String> getPosts(String name) {
+		String targetShard = MapShards.whichShard(name);
+		
+		List<String> posts = new ArrayList<String>();
+		
+		try {
+			String sql = "SELECT * FROM post WHERE name = ?";
+			PreparedStatement s = createConnection(targetShard).prepareStatement(sql);
+			s.setString(0, name);
+			ResultSet rs = s.executeQuery();
+			while(rs.next()) {
+				String message = rs.getString("message");
+				posts.add(message);
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return posts;
+	}
+	
+	private static ResultSet queryShard(String shard, String query) throws ClassNotFoundException, SQLException {
+		List<Object[]> result = new ArrayList<Object[]>();
+		
+		Statement s = createConnection(shard).createStatement();
+	    ResultSet rs = s.executeQuery(query);
+	    
+	    return rs;
+	}
+	
+	private static String user = "plibin";
+	private static String password  = "plibin";
+	private static Connection createConnection(String shard) throws ClassNotFoundException, SQLException {
+	      Class.forName("com.mysql.jdbc.Driver");
+	      Connection connect = DriverManager
+	          .getConnection(
+	        		  "jdbc:mysql://" + shard + "/feedback?" + "user=" + user + "&password=" + password);
+
+	      return connect;
+	}
+}
---
+++ src/shard/src/ossec/shards/Commands.java
@@ -0,0 +1,27 @@
+package ossec.shards;
+
+public class Commands {
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			System.err.println("Possible commands: add-user user, post user message, get-posts user, users-that-posted-pattern pattern");
+			return;
+		}
+		
+		String command = args[0];
+		if ("add-user".equals(command)) {
+			Actions.addUser(args[1]);
+		} else if ("post".equals(command)) {
+			Actions.post(args[1], args[2]);
+		} else if ("get-posts".equals(command)) {
+			System.out.println("User " + args[1] + " posted:");
+			for (String message : Actions.getPosts(args[1])) {
+				System.out.println(message);
+			}
+		} else if ("users-that-posted-pattern".equals(command)) {
+			System.out.println("Users that posted posts with this pattern( " + args[1] + "):");
+			for (String user : Actions.findUsersThatPostedPattern(args[1])) {
+				System.out.println(user);
+			}
+		}
+	}
+}
