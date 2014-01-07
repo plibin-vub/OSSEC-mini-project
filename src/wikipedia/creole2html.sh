3cd8f9f24ff5476242fa71c16acc0a2614b861dc
Pieter Libin <pieter.libin@rega.kuleuven.be>
2014-01-07 20:46:01 +0100

    add support for internal links

--- src/wikipedia/creole2html.sh
+++ src/wikipedia/creole2html.sh
@@ -5,3 +5,9 @@ BASE_FILE=`basename $CREOLE_FILE .crl`
 HTML=".html"
 HTML_FILE="$BASE_FILE$HTML"
 txt2tags --target=html --outfile="$BASE_FILE.html" "$CREOLE_FILE"
+
+#txt2tags does not handle internal links properly:
+#I replace them here with this quick regexp hack, however,
+#this is not production code, since it does not handle special characters such
+#as spaces properly
+sed --in-place -e 's/\[\[\([^]]*\)\]\]/\\<a href="\1">\1<\a>/g'
