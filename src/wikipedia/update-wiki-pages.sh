f490e0a19c2a322cb392ee3478ceb32c11812699
plibin <pieter.libin@vub.ac.be>
2014-01-07 20:03:39 +0100

    fixes

--- src/wikipedia/update-wiki-pages.sh
+++ src/wikipedia/update-wiki-pages.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#pull from 'content's git repository
-cd ~/wiki-pages
-git pull
-
 #get this scripts directory location
 #add reference to stackoverflow article
 DIR="$(cd "$(dirname "$0")" && pwd )"
 
+#pull from 'content's git repository
+cd ~/wiki-pages
+git pull
+
 #convert all files from 'creole' to HTML
-find . -name '*.crl' -exec $DIR/creole_to_html.sh {} \;
+find . -name '*.crl' -exec $DIR/creole2html.sh {} \;
